597a954f2857621e6448936853a28b1fb7f57671
bchelkowski <bchelkow@gmail.com>
2026-06-17 11:01:13 +0200

    chore(brightscript-parser): remove dist from git tracking

--- packages/brightscript-parser/dist/src/diagnostics.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Parse diagnostics — errors and warnings produced by the parser.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=diagnostics.js.map
